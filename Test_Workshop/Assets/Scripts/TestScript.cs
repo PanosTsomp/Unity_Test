85987728f834cdd42e40c4d979b58193e777b579
matamike <matamike1990@gmail.com>
2025-04-22 20:52:17 +0300

    Remove unused comment (TEST).

--- Test_Workshop/Assets/Scripts/TestScript.cs
+++ Test_Workshop/Assets/Scripts/TestScript.cs
@@ -16,7 +16,6 @@ public class TestScript : MonoBehaviour
         targetFaceDilate = endFaceDilate;
         targetOutlineWidth = endOutlineWidth;
         material = textMeshPro.fontMaterial;
-        //Some change common 
     }
 
     void Update()
